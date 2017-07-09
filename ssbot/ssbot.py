5120a9bd49547659c681d0c46f7d9696b9cf229f
Kien Nguyen <kiennt@vn.fujitsu.com>
2017-07-09 16:40:44 +0700

    Update async way, not work perfectly

--- ssbot/ssbot.py
+++ ssbot/ssbot.py
@@ -1,10 +1,12 @@
+import asyncio
 import time
+import datetime as dt
 from slackclient import SlackClient
 
 from config import BOT_ID, BOT_TOKEN
 
 
-AT_BOT = '<@%s>'% BOT_ID
+AT_BOT = '<@%s>' % BOT_ID
 EXAMPLE_COMMAND = 'do'
 slack_client = SlackClient(BOT_TOKEN)
 READ_WEBSOCKET_DELAY = 1
@@ -36,12 +38,12 @@ def parse_slack_output(slack_rtm_output):
             if output and 'text' in output and AT_BOT in output['text']:
                 # return text after the @ mention, whitespace removed
                 return output['text'].split(AT_BOT)[1].strip().lower(), \
-                       output['channel']
+                    output['channel']
     return None, None
 
 
-if __name__ == "__main__":
-    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
+def listen_sync():
+    READ_WEBSOCKET_DELAY = 1  # 1 second delay between reading from firehose
     if slack_client.rtm_connect():
         print("StarterBot connected and running!")
         while True:
@@ -51,3 +53,33 @@ if __name__ == "__main__":
             time.sleep(READ_WEBSOCKET_DELAY)
     else:
         print("Connection failed. Invalid Slack token or bot ID?")
+
+
+@asyncio.coroutine
+def listen_async():
+    yield from asyncio.sleep(1)
+    try:
+        slack_client.rtm_connect()
+        info = slack_client.rtm_read()
+        if len(info) > 0:
+            if 'text' in info[0] and 'channel' in info[0]:
+                print(info)
+                resp = dt.datetime.strftime(dt.datetime.now(), '%H:%M:%S')
+                channel = info[0]['channel']
+                slack_client.rtm_send_message(channel, resp)
+    except Exception as e:
+        pass
+    finally:
+        asyncio.async(listen_async())
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    try:
+        asyncio.async(listen_async())
+        loop.run_forever()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print('Loop.close()')
+        loop.close()
