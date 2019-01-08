a88a78704f2bbd775dd6e09c3e29db9335389ec7
Kien Nguyen <kiennt65@viettel.com.vn>
2019-01-08 15:38:30 +0700

    Update golang grpc testing

---
+++ grpc/golang_example/client/main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	pb "github.com/ntk148v/testing/grpc/golang_example/customer"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"io"
+	"log"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+// createCustomer calls the RPC method CreateCustomer of CustomerServer
+func createCustomer(client pb.CustomerClient, customer *pb.CustomerRequest) {
+	resp, err := client.CreateCustomer(context.Background(), customer)
+	if err != nil {
+		log.Fatalf("Could not create Customer: %v", err)
+	}
+	if resp.Success {
+		log.Printf("A new Customer has been added with id: %d", resp.Id)
+	}
+}
+
+// getCustomers calls the RPC method GetCustomer of CustomerServer
+func getCustomers(client pb.CustomerClient, filter *pb.CustomerFilter) {
+	// calling the streaming API
+	stream, err := client.GetCustomer(context.Background(), filter)
+	if err != nil {
+		log.Fatalf("Error on get customers: %v", err)
+	}
+	for {
+		customer, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("%v.GetCustomer(_) = _, %v", client, err)
+		}
+		log.Printf("Customer: %v", customer)
+	}
+}
+
+func main() {
+	// Set up a connection to the gRPC server
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	// Creates a new CustomerClient
+	client := pb.NewCustomerClient(conn)
+
+	customer := &pb.CustomerRequest{
+		Id:    101,
+		Name:  "Kien Nguyen-Tuan",
+		Email: "kiennt2609@gmail",
+		Phone: "976872724",
+		Address: []*pb.CustomerRequest_Address{
+			&pb.CustomerRequest_Address{
+				Street:            "18 HHT",
+				City:              "Hanoi",
+				State:             "Hanoi",
+				Zip:               "6969",
+				IsShippingAddress: false,
+			},
+			&pb.CustomerRequest_Address{
+				Street:            "O day",
+				City:              "Thanh pho ne",
+				State:             "Ne thanh pho",
+				Zip:               "9696",
+				IsShippingAddress: true,
+			},
+		},
+	}
+
+	// Create a new customer
+	createCustomer(client, customer)
+
+	customer = &pb.CustomerRequest{
+		Id:    102,
+		Name:  "Kien Nguyen",
+		Email: "mail@mail.com",
+		Phone: "932245949",
+		Address: []*pb.CustomerRequest_Address{
+			&pb.CustomerRequest_Address{
+				Street:            "Met vl",
+				City:              "Haloi",
+				State:             "Haloi",
+				Zip:               "6999",
+				IsShippingAddress: true,
+			},
+		},
+	}
+
+	// Create one more new customer
+	createCustomer(client, customer)
+	// filter with an empty Keyword
+	filter := &pb.CustomerFilter{Keyword: ""}
+	getCustomers(client, filter)
+}
