e1d5fd53e7366fd751a2d586aaac986669693d5b
Kien Nguyen <kiennt65@viettel.com.vn>
2019-05-31 15:30:31 +0700

    Add retry example

--- golang/retry/retry_http.go
+++ golang/retry/retry_http.go
@@ -1,3 +1,34 @@
+package httpretry
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func retry(attempts int, sleep time.Duration, f func() error) error {
+	if err := f(); err != nil {
+		if s, ok := err.(stop); ok {
+			// Return the original error for later checking
+			return s.error
+		}
+
+		if attempts--; attempts > 0 {
+			// Add some randomness to prevent creating a Thundering Herd
+			jitter := time.Duration(rand.Int63n(int64(sleep)))
+			sleep = sleep + jitter/2
+
+			time.Sleep(sleep)
+			return retry(attempts, 2*sleep, f)
+		}
+		return err
+	}
+
+	return nil
+}
+
+type stop struct {
+	error
+}
+
 // DeleteThing attempts to delete a thing. It will try a maximum of three times.
 func DeleteThing(id string) error {
 	// Build the request
