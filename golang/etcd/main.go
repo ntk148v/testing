bb71ae11cc454ec6d89f3746f67ba5250b94a958
Kien Nguyen <kiennt65@viettel.com.vn>
2019-09-19 09:38:13 +0700

    Update

--- golang/etcd/main.go
+++ golang/etcd/main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"time"
 
 	"go.etcd.io/etcd/clientv3"
 )
+
+type Person struct {
+	Name string
+	Age  int
+}
 
 func main() {
 	cli, err := clientv3.New(clientv3.Config{
@@ -40,5 +46,51 @@ func main() {
 	for _, ev := range resp.Kvs {
 		fmt.Printf("%s : %s\n", ev.Key, ev.Value)
 	}
+
+	p := Person{
+		Name: "Kien",
+		Age:  26,
+	}
+
+	value, err := json.Marshal(&p)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = cli.Put(ctx, "/cloud/test1", string(value))
+	if err != nil {
+		panic(err)
+	}
+
+	p = Person{
+		Name: "Kien Khac",
+		Age:  69,
+	}
+
+	value, err = json.Marshal(&p)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = cli.Put(ctx, "/cloud/test2", string(value))
+	if err != nil {
+		panic(err)
+	}
+
+	resp, err = cli.Get(ctx, "cloud", clientv3.WithPrefix())
+	if err != nil {
+		panic(err)
+	}
+
+	for _, ev := range resp.Kvs {
+		fmt.Printf("%s : %s\n", ev.Key, ev.Value)
+		// p = Person{}
+		// err = json.Unmarshal(ev.Value, &p)
+		// if err != nil {
+		// 	panic(err)
+		// }
+		// fmt.Printf("%+v", p)
+	}
+
 	cancel()
 }
