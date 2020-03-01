650ac1d31fa40730b38dca0c67e2d62c1a7f9f52
Kien Nguyen <kiennt65@viettel.com.vn>
2020-03-01 12:17:41 +0700

    Add zeep test

---
+++ python/zeep/main.py
@@ -0,0 +1,26 @@
+import logging
+import os
+import sys
+
+from zeep import Client
+
+LOG = logging.getLogger(__name__)
+
+
+def setup_logging():
+    stdout_handler = logging.StreamHandler(sys.stdout)
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        handlers=[stdout_handler])
+
+
+if __name__ == '__main__':
+    setup_logging()
+    kwargs = {}
+    try:
+        kwargs['gnoc_url'] = os.environ.get('gnoc_url')
+    except KeyError as e:
+        LOG.error('Missing environment variables! %s' % str(e))
+    soap_client = Client(kwargs['gnoc_url'])
+    LOG.info(soap_client.service.getListSR())
