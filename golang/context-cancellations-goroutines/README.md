55b584ebc079b5cd75695461be56ecbd9d88d539
Kien Nguyen <kiennt65@viettel.com.vn>
2020-03-18 11:14:44 +0700

    testing context

---
+++ golang/context-cancellations-goroutines/README.md
@@ -0,0 +1,3 @@
+# Context and cancellations of goroutines
+
+http://dahernan.github.io/2015/02/04/context-and-cancellation-of-goroutines/
---
+++ golang/context-cancellations-goroutines/origin/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var (
+	wg sync.WaitGroup
+)
+
+func work() error {
+	defer wg.Done()
+
+	for i := 0; i < 1000; i++ {
+		select {
+		case <-time.After(2 * time.Second):
+			fmt.Println("Doing some work ", i)
+		}
+	}
+	return nil
+}
+
+func main() {
+	fmt.Println("Hey, I'm going to do some work")
+
+	wg.Add(1)
+	go work()
+	wg.Wait()
+
+	fmt.Println("Finished. I'm going home")
+}
