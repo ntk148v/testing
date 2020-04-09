1c23d1b8a364783417dd6850e4a7fae9faf8a56d
Kien Nguyen <kiennt65@viettel.com.vn>
2020-04-09 14:04:42 +0700

    Add github module test

---
+++ golang/github/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
+)
+
+func main() {
+	token := os.Getenv("TOKEN")
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: token},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+
+	client := github.NewClient(tc)
+
+	// list all repositories for the authenticated user
+	repos, _, err := client.Repositories.List(ctx, "", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(repos)
+}
