8b305a30ab223f52efe464ee4a0d35832074e0af
Kien Nguyen <kiennt65@viettel.com.vn>
2020-07-16 13:58:59 +0700

    update

--- golang/github-scratch/main.go
+++ golang/github-scratch/main.go
@@ -9,36 +9,46 @@ import (
 	"os"
 )
 
+type Label struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Color       string `json:"color"`
+}
+
 func main() {
-	owner := "ntk148v"
-	repo := "testing"
+	owner := "vCloud-DFTBA"
+	repo := "faythe"
 	url := fmt.Sprintf("https://api.github.com/repos/%s/%s/labels", owner, repo)
-	values := map[string]string{
-		"name":        "newbug",
-		"description": "Something isn't working",
-		"color":       "f29513",
-	}
-	jsonValue, _ := json.Marshal(values)
-	token := "token " + os.Getenv("TOKEN")
-	// Create a new request using http
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonValue))
 
-	// add authorization header to the req
-	req.Header.Add("Authorization", token)
-
-	// Send req using http Client
-	client := &http.Client{}
-	resp, err := client.Do(req)
+	file, err := ioutil.ReadFile("/tmp/labels.json")
 	if err != nil {
 		panic(err)
 	}
-	body, err := ioutil.ReadAll(resp.Body)
-	fmt.Print(string(body))
-	resp, err = http.Get(url)
-	if err != nil {
-		panic(err)
+	labels := make([]Label, 10)
+	_ = json.Unmarshal([]byte(file), &labels)
+	for _, l := range labels {
+		jsonValue, _ := json.Marshal(l)
+		token := "token " + os.Getenv("TOKEN")
+		// Create a new request using http
+		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonValue))
+
+		// add authorization header to the req
+		req.Header.Add("Authorization", token)
+
+		// Send req using http Client
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		body, err := ioutil.ReadAll(resp.Body)
+		fmt.Print(string(body))
+		resp, err = http.Get(url)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+		body, err = ioutil.ReadAll(resp.Body)
+		fmt.Print(string(body))
 	}
-	defer resp.Body.Close()
-	body, err = ioutil.ReadAll(resp.Body)
-	fmt.Print(string(body))
 }
