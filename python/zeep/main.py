2b3235a63877ee76a79d29bd9ee676589d108f8b
Kien Nguyen <kiennt65@viettel.com.vn>
2020-09-14 12:30:43 +0700

    update testing

--- python/zeep/main.py
+++ python/zeep/main.py
@@ -22,10 +22,17 @@ if __name__ == '__main__':
         kwargs['gnoc_url'] = os.environ.get('GNOC_URL')
     except KeyError as e:
         LOG.error('Missing environment variables! %s' % str(e))
-    soap_client = Client(kwargs['gnoc_url'])
-    srdto_type = soap_client.get_type('ns0:srdto')
+    sr_service_client = Client(
+        '{}/{}?wsdl'. format(kwargs['gnoc_url'], 'SRService'))
+    srdto_type = sr_service_client.get_type('ns0:srdto')
     srdto_obj = srdto_type(country='281',
-                           offset=100,
-                           srId='26347',)
-    LOG.info(soap_client.service.getListSR(rowStart=0, maxRow=100,
-                                           srDTO=srdto_obj))
+                           offset=100)
+    list_sr = sr_service_client.service.getListSR(rowStart=0, maxRow=100,
+                                                  srDTO=srdto_obj)
+    sr_file_service_client = Client(
+        '{}/{}?wsdl'. format(kwargs['gnoc_url'], 'SRFileService'))
+    for sr in list_sr:
+        sr_file = sr_file_service_client.service.getListFileSR(srId=sr['srId'])
+        if sr_file:
+            LOG.info(sr_file)
+            break
