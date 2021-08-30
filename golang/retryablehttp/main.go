720670e903372767634c6764a330cc9188e1d7aa
Kien Nguyen <kiennt2609@gmail.com>
2021-08-30 11:49:33 +0700

    Test

--- golang/retryablehttp/main.go
+++ golang/retryablehttp/main.go
@@ -1,17 +1,54 @@
 package main
 
 import (
+	"crypto/tls"
+	"encoding/base64"
 	"fmt"
+	"net"
+	"net/http"
 	"os"
+	"runtime"
+	"time"
 
 	"github.com/hashicorp/go-retryablehttp"
 )
 
+func defaultPooledTransport() *http.Transport {
+	transport := &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
+		DialContext: (&net.Dialer{
+			Timeout:   30 * time.Second,
+			KeepAlive: 30 * time.Second,
+			DualStack: true,
+		}).DialContext,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
+		TLSHandshakeTimeout:   10 * time.Second,
+		ExpectContinueTimeout: 1 * time.Second,
+		MaxIdleConnsPerHost:   runtime.GOMAXPROCS(0) + 1,
+		TLSClientConfig:       &tls.Config{InsecureSkipVerify: true},
+	}
+	return transport
+}
+
+func basicAuth(username, password string) string {
+	auth := username + ":" + password
+	return base64.StdEncoding.EncodeToString([]byte(auth))
+}
+
 func main() {
 	url := os.Getenv("URL")
-	resp, err := retryablehttp.Get(url)
+	username := os.Getenv("USERNAME")
+	password := os.Getenv("PASSWORD")
+	client := retryablehttp.NewClient()
+	client.HTTPClient = &http.Client{
+		Transport: defaultPooledTransport(),
+	}
+	req, err := retryablehttp.NewRequest("GET", url, nil)
+	req.Header.Add("Authorization", "Basic"+basicAuth(username, password))
+	resp, err := client.Do(req)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(resp)
+	fmt.Println(resp.Request.URL.String())
 }
