46c3ef3b70515e453aa55682c121e8d33af25bba
Kien Nguyen <kiennt2609@gmail.com>
2021-10-25 09:13:50 +0700

    Add http2

---
+++ golang/http2/server.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	// Create a server on port 8000
+	// Exactly how you would run an HTTP/1.1 server
+	srv := &http.Server{Addr: ":8000", Handler: http.HandlerFunc(handle)}
+
+	// Start the server with TLS, since we are running HTTP/2 it must be
+	// run with TLS.
+	// Exactly how you would run an HTTP/1.1 server with TLS connection.
+	log.Printf("Serving on https://0.0.0.0:8000")
+	log.Fatal(srv.ListenAndServeTLS("server.crt", "server.key"))
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	// Log the request protocol
+	log.Printf("Got connection: %s", r.Proto)
+	// Send a message back to the client
+	w.Write([]byte("Hello"))
+}
