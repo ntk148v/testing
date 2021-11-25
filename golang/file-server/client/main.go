c9a8bb5fc10202ebf70ddebfbc932a49f0b18abf
Kien Nguyen <kiennt2609@gmail.com>
2021-11-25 15:27:54 +0700

    Attempt to parallel download
    but failed...

--- golang/file-server/client/main.go
+++ golang/file-server/client/main.go
@@ -49,8 +49,9 @@ func main() {
 	} else if accept_ranges[0] != "bytes" {
 		log.Fatalln("Support `Accept-Ranges`, but value is not `bytes`")
 	}
-	fileSize, _ := strconv.ParseInt(headers["Content-Length"][0], 10, 64)
-	partialSize := int(fileSize) / *count
+	fileSizeTmp, _ := strconv.ParseInt(headers["Content-Length"][0], 10, 64)
+	fileSize := int(fileSizeTmp)
+	partialSize := fileSize / *count
 	// Get the 1st part
 	req.Header.Add("Range", fmt.Sprintf("bytes=%d-%d", 0, partialSize))
 	resp, err = client.Do(req)
@@ -60,15 +61,92 @@ func main() {
 	defer resp.Body.Close()
 
 	f, err := os.OpenFile("/tmp/1stpart", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	defer f.Close()
 	if err != nil {
 		panic(err)
 	}
+	defer f.Close()
+
 	// No handle at all for testing
 	written, err := io.Copy(f, io.TeeReader(resp.Body, &WriteCounter{}))
 	if err != nil {
 		panic(err)
 	}
 	log.Println("")
-	log.Printf("Downloaded %d MB of total %d MB", written/1024, fileSize/1024)
+	log.Printf("Downloaded 1st part %d MB of total %d MB", written/1024, fileSize/1024)
+
+	// // Parallel download
+	// var (
+	// 	start, end int
+	// 	wg         sync.WaitGroup
+	// )
+
+	// f, err = os.OpenFile("/tmp/parallel", os.O_CREATE|os.O_WRONLY, 0666)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer f.Close()
+
+	// for i := 0; i < *count; i++ {
+	// 	if i == *count {
+	// 		end = int(fileSize)
+	// 	} else {
+	// 		end = start + partialSize
+	// 	}
+
+	// 	wg.Add(1)
+	// 	go func(partIndex, start, end int, wg sync.WaitGroup) {
+	// 		defer wg.Done()
+	// 		// Perform request
+	// 		reqPart, err := http.NewRequest("GET", *url, nil)
+	// 		if err != nil {
+	// 			log.Printf("Error - part %d: %s\n", partIndex, err)
+	// 			return
+	// 		}
+
+	// 		req.Header.Add("Range", fmt.Sprintf("bytes=%d-%d", start, end))
+	// 		respPart, err := client.Do(reqPart)
+	// 		if err != nil {
+	// 			log.Printf("Error - part %d: %s\n", partIndex, err)
+	// 			return
+	// 		}
+
+	// 		defer respPart.Body.Close()
+	// 		partSize, _ := strconv.ParseInt(resp.Header["Content-Length"][0], 10, 64)
+	// 		log.Printf("Downloading part %d [%d-%d] size %d\n", partIndex, start, end, partSize/1024)
+
+	// 		// make a buffer to keep chunks that are read
+	// 		buf := make([]byte, 32*1024)
+	// 		for {
+	// 			nr, er := respPart.Body.Read(buf)
+	// 			if nr > 0 {
+	// 				nw, ew := f.WriteAt(buf[0:nr], int64(start))
+	// 				if nw < 0 || nr < nw {
+	// 					nw = 0
+	// 					if ew == nil {
+	// 						log.Fatalf("Error - part %d: invalid write\n", partIndex)
+	// 						break
+	// 					}
+	// 				}
+	// 				if nr != nw {
+	// 					log.Fatalf("Error - part %d, short writting\n", partIndex)
+	// 				}
+
+	// 				start += nw
+	// 			}
+
+	// 			if er != nil {
+	// 				log.Printf("Part %d got err %s", partIndex, er)
+	// 				if er == io.EOF || er.Error() == "EOF" {
+	// 					// Download successfully
+	// 					break
+	// 				}
+	// 				log.Fatalf("Error - part %d: %s\n", partIndex, err)
+	// 			}
+	// 		}
+	// 	}(i, start, end, wg)
+	// 	start = end
+	// }
+
+	// wg.Wait()
+	// log.Println("Downloaded")
 }
