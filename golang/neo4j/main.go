f73b5a84f39227a5dfbd2ebd410394e41eafabd2
Kien Nguyen <kiennt2609@gmail.com>
2021-12-29 15:45:04 +0700

    Update neo4j tgest

--- golang/neo4j/main.go
+++ golang/neo4j/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+	"strings"
 
 	"github.com/neo4j/neo4j-go-driver/v4/neo4j"
 )
@@ -10,57 +12,34 @@ import (
 func main() {
 	// Neo4j 4.0, defaults to no TLS therefore use bolt:// or neo4j://
 	// Neo4j 3.5, defaults to self-signed certificates, TLS on, therefore use bolt+ssc:// or neo4j+ssc://
-	driver, err := neo4j.NewDriver(os.Getenv("NEO4J_URL"), neo4j.BasicAuth(os.Getenv("NEO4J_USERNAME"), os.Getenv("NEO4J_PASSWORD"), ""))
+	driver, err := neo4j.NewDriver(os.Getenv("NEO4J_URI"), neo4j.BasicAuth(os.Getenv("NEO4J_USERNAME"), os.Getenv("NEO4J_PASSWORD"), ""))
 	if err != nil {
 		panic(err)
 	}
 	// Handle driver lifetime based on your application lifetime requirements  driver's lifetime is usually
 	// bound by the application lifetime, which usually implies one driver instance per application
 	defer driver.Close()
-	item, err := insertItem(driver)
+
+	// Load cypher commands from files
+	buf, err := ioutil.ReadFile(os.Getenv("NEO4J_CYPHER_FILE"))
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("%+v\n", item)
-}
+	cypher := string(buf)
 
-func insertItem(driver neo4j.Driver) (*Item, error) {
-	// Sessions are short-lived, cheap to create and NOT thread safe. Typically create one or more sessions
-	// per request in your web application. Make sure to call Close on the session when done.
-	// For multi-database support, set sessionConfig.DatabaseName to requested database
-	// Session config will default to write mode, if only reads are to be used configure session for
-	// read mode.
+	// Create session
 	session := driver.NewSession(neo4j.SessionConfig{})
 	defer session.Close()
-	result, err := session.WriteTransaction(createItemFn)
-	if err != nil {
-		return nil, err
-	}
-	return result.(*Item), nil
-}
 
-func createItemFn(tx neo4j.Transaction) (interface{}, error) {
-	records, err := tx.Run("CREATE (n:Item { id: $id, name: $name }) RETURN n.id, n.name", map[string]interface{}{
-		"id":   1,
-		"name": "Item 1",
-	})
-	// In face of driver native errors, make sure to return them directly.
-	// Depending on the error, the driver may try to execute the function again.
-	if err != nil {
-		return nil, err
+	for _, query := range strings.SplitAfter(cypher, ";") {
+		if query == "\n" || query == "" {
+			continue
+		}
+		fmt.Println("Execute query", query)
+		if _, err := session.WriteTransaction(func(tx neo4j.Transaction) (interface{}, error) {
+			return tx.Run(query, nil)
+		}); err != nil {
+			panic(err)
+		}
 	}
-	record, err := records.Single()
-	if err != nil {
-		return nil, err
-	}
-	// You can also retrieve values by name, with e.g. `id, found := record.Get("n.id")`
-	return &Item{
-		Id:   record.Values[0].(int64),
-		Name: record.Values[1].(string),
-	}, nil
-}
-
-type Item struct {
-	Id   int64
-	Name string
 }
