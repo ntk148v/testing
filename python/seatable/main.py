27f0da83b42537e13f15468631be87f717585d8f
Kien Nguyen <kiennt2609@gmail.com>
2022-03-31 11:24:50 +0700

    Add seatable example

---
+++ python/seatable/main.py
@@ -0,0 +1,12 @@
+import os
+
+from seatable_api import Base
+
+try:
+    server_url = os.environ['SEATABLE_SERVER_URL']
+    api_token = os.environ['SEATABLE_API_TOKEN']
+except KeyError as err:
+    exit('Required environment variables is missing: %s' % (err))
+
+base = Base(api_token, server_url)
+base.auth(with_socket_io=True)
