e9ff22ee0118d2df723e3446d774849572b9bec8
Kien Nguyen <kiennt2609@gmail.com>
2022-07-21 18:28:54 +0700

    update

--- terraform/docker/learn-terraform-docker-container/main.tf
+++ terraform/docker/learn-terraform-docker-container/main.tf
@@ -16,7 +16,7 @@ resource "docker_image" "nginx" {
 
 resource "docker_container" "nginx" {
   image = docker_image.nginx.latest
-  name  = "tutorial"
+  name  = var.container_name
   ports {
     internal = 80
     external = 8000
