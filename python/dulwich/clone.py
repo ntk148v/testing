c958bb8ea2eedd604a18e2b05566293e2b1d64b6
Kien Nguyen <kiennt2609@gmail.com>
2022-10-04 18:05:27 +0700

    Test dulwich

---
+++ python/dulwich/clone.py
@@ -0,0 +1,35 @@
+
+"""Clone.
+This trivial script demonstrates how to clone or lock a remote repository.
+Example usage:
+  1. python clone.py git://github.com/jelmer/dulwich
+  2. python clone.py git://github.com/jelmer/dulwich.git
+  3. python clone.py git://github.com/jelmer/dulwich.git dulwich
+  4. python clone.py https://github.com/jelmer/dulwich.git
+"""
+
+
+import sys
+
+from os.path import basename
+
+from getopt import getopt
+
+from dulwich import porcelain
+
+
+_, args = getopt(sys.argv, "", [])
+
+
+if len(args) < 2:
+    print("usage: %s host:path path" % (args[0], ))
+    sys.exit(1)
+
+elif len(args) < 3:
+    target_path = basename(args[1].split(":")[-1])
+    if target_path[-4:] == ".git":
+        target_path = target_path[:-4]
+else:
+    target_path = args[2]
+
+porcelain.clone(args[1], target_path)
