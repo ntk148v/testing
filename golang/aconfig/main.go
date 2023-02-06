b5a0a0bf9aebb77ac9a287cc82ae2047ed4b7c8d
Kien Nguyen <kiennt2609@gmail.com>
2023-02-06 16:54:48 +0700

    test: add aconfig

---
+++ golang/aconfig/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/cristalhq/aconfig"
+)
+
+type MyConfig struct {
+	HTTPPort int `default:"1111" usage:"just a number"`
+	Auth     struct {
+		User string `default:"def-user" usage:"your user"`
+		Pass string `default:"def-pass" usage:"make it strong"`
+	}
+}
+
+func main() {
+	var cfg MyConfig
+	loader := aconfig.LoaderFor(&cfg, aconfig.Config{
+		SkipDefaults: true,
+		SkipFiles:    true,
+		SkipEnv:      true,
+		SkipFlags:    true,
+		Files:        []string{"/var/opt/myapp/config.json"},
+		EnvPrefix:    "APP",
+		FlagPrefix:   "app",
+	})
+
+	if err := loader.Load(); err != nil {
+		log.Panic(err)
+	}
+
+	fmt.Println("* Simple:")
+	fmt.Printf("HTTPPort:  %v\n", cfg.HTTPPort)
+	fmt.Printf("Auth.User: %q\n", cfg.Auth.User)
+	fmt.Printf("Auth.Pass: %q\n", cfg.Auth.Pass)
+
+	fmt.Println("* WalkFields")
+	loader = aconfig.LoaderFor(&cfg, aconfig.Config{
+		SkipFiles: true,
+		SkipEnv:   true,
+		SkipFlags: true,
+	})
+
+	loader.WalkFields(func(f aconfig.Field) bool {
+		fmt.Printf("%v: %q %q %q %q\n", f.Name(), f.Tag("env"), f.Tag("flag"), f.Tag("default"), f.Tag("usage"))
+		return true
+	})
+
+}
