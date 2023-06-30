58a204e8e6d20105c34012a883a642ca281b0b40
Kien Nguyen <kiennt2609@gmail.com>
2023-06-30 17:04:22 +0700

    test: dotnet redis

---
+++ dotnet/redis/Program.cs
@@ -0,0 +1,29 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Threading.Tasks;
+
+namespace ReferenceConsoleRedisApp
+{
+    class Program
+    {
+        // ConnectionMultiplexer must be shared and reused within a runtime.
+        // It’s recommended that you use dependency injection to pass it where it’s needed
+        static readonly ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(
+            new ConfigurationOptions
+            {
+                // Sammple REDIS_URI string: https://stackexchange.github.io/StackExchange.Redis/Configuration
+                EndPoints = { System.Environment.GetEnvironmentVariable("REDIS_URI") ?? "localhost" },
+            });
+
+        static async Task Main(string[] args)
+        {
+            var db = redis.GetDatabase();
+            var pong = await db.PingAsync();
+            Console.WriteLine(pong);
+
+            string value = "sample-value";
+            Console.WriteLine("Set Value: " + db.StringSet("sample-key", value));
+            Console.WriteLine("Get Value: " + db.StringGet("sample-key"));
+        }
+    }
+}
---
+++ dotnet/redis/README.md
@@ -0,0 +1,5 @@
+# Redis client
+
+```shell
+docker run --name redis -d -e ALLOW_EMPTY_PASSWORD=yes -p 6379:6379 bitnami/redis:latest
+```
