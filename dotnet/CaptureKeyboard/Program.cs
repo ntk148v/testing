fe557202e8a40203e0c6ffb5cca29e8431eb04d2
Kien Nguyen <kiennt2609@gmail.com>
2023-08-24 15:51:46 +0700

    update

---
+++ dotnet/CaptureKeyboard/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    public class Prorgam
+    {
+        private static readonly AutoResetEvent _closing = new AutoResetEvent(false);
+
+        public static void Main(string[] args)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                while (true)
+                {
+                    Console.WriteLine(DateTime.Now.ToString());
+                    Thread.Sleep(1000);
+                }
+            });
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(OnExit);
+            _closing.WaitOne();
+        }
+
+        protected static void OnExit(object sender, ConsoleCancelEventArgs args)
+        {
+            Console.WriteLine("Exit");
+            _closing.Set();
+        }
+    }
+}
