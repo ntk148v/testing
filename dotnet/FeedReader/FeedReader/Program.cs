7df91e99445a5720c01773daf51e72e6e1d66b00
Kien Nguyen <kiennt2609@gmail.com>
2023-09-27 14:59:38 +0700

    test: feedreader is not working, post is missing name, ...

---
+++ dotnet/FeedReader/FeedReader/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace FeedReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var url = "https://khalidabuhakmeh.com/feed.xml";
+            using (var reader = XmlReader.Create(url)){
+                var feed = SyndicationFeed.Load(reader);
+
+                var post = feed
+                    .Items
+                    .FirstOrDefault();
+                Console.WriteLine(post.Content);
+            }
+        }
+    }
+}
