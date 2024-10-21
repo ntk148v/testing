4877b733c8efd992cea71bd47f139d0bc8ec47b7
Kien Nguyen Tuan <kiennt98@fpt.com>
2024-10-21 15:49:57 +0700

    test

---
+++ python/prefect/github_star.py
@@ -0,0 +1,21 @@
+from prefect import flow, task
+from typing import List
+import httpx
+
+
+@task(log_prints=True)
+def get_stars(repo: str):
+    url = f"https://api.github.com/repos/{repo}"
+    count = httpx.get(url).json()["stargazers_count"]
+    print(f"{repo} has {count} stars!")
+
+
+@flow(name="GitHub Stars")
+def github_stars(repos: List[str]):
+    for repo in repos:
+        get_stars(repo)
+
+
+# run the flow!
+if __name__=="__main__":
+    github_stars(["PrefectHQ/Prefect", "ntk148v/til"])
